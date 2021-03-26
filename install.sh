165a11981fc55d466841b90ccefc6e38db2328ce
Douglas Souza <douglas.souza.002@acad.pucrs.br>
2021-03-25 22:18:02 -0300

    Update

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ sudo su --preserve-environment
 # ------- apt installs -------
 apt update && apt upgrade
 
-wget -O- http://neuro.debian.net/lists/focal.us-nh.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
-apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 add-apt-repository -y ppa:git-core/ppa  # latest git
 add-apt-repository -y ppa:libratbag-piper/piper-libratbag-git
 apt update
@@ -18,12 +16,15 @@ aot install -y build-essential
 apt install -y curl
 apt install -y exfat-utils exfat-fuse  # exfat filesystem stuff
 apt install -y git
-apt install -y git-annex
 apt install -y gparted  # disk partitioner
 apt install -y piper # mouse settings
 apt install -y terminator  # better terminal app
 apt install -y unrar  # support for .rar archives
 apt install -y wget
+# latest git-annex
+wget -O- http://neuro.debian.net/lists/focal.us-nh.libre | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+apt install -y git-annex
 # prepare dir for latex
 mkdir -p /usr/local/texlive
 exit # leave root user
@@ -53,4 +54,7 @@ rm ~/Downloads/Anaconda3-2020.11-Linux-x86_64.sh
 echo 'ZSH_THEME="bira"' >> ~/.zshrc
 
 # code extensions
-code --install-extension PKief.material-icon-theme
+code --install-extension pkief.material-icon-theme
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
